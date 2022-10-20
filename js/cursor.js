b671d6b440d6fe7edf817befa7a084b1fb231e0d
nawam-karki <nawam@psychoactive.co.nz>
2022-10-21 10:01:37 +1300

    projects page

--- js/cursor.js
+++ js/cursor.js
@@ -18,7 +18,7 @@ const initCursor = () => {
     });
 
     let cursorTriggers = document.querySelectorAll(
-      "a, .he-nav-link, .he-project-title,  .he-cross, .simplebar-track, .he-subnav-spacer-wrap, .hamburger-wrapper, .nav-item"
+      "a, .he-nav-link, .he-project-title,  .he-cross, .simplebar-track, .he-subnav-spacer-wrap, .hamburger-wrapper, .nav-item, .he-projects-wrap"
     );
 
     cursorTriggers.forEach((t) => {
